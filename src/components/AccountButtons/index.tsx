2da21b887e98548a0576cf3404decf09c47d7366
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-02-16 16:11:30 +0000

    Update index.tsx

--- src/components/AccountButtons/index.tsx
+++ src/components/AccountButtons/index.tsx
@@ -2,13 +2,15 @@ import {Component} from 'solid-js';
 
 const AccountButtons : Component = () => {
   return <div class="flex items-center font-medium">
-    {/* Link Button */}
-    <a href="https://rock-swap.io/#/swap?outputCurrency=0x2ae505d947254bd7fde48799cc388215348e1e23" class="hover:text-blue transition-all duration-300">.</a>
-    {/* Separator */}
-    <span class="mx-6 text-white/20 font-thin">|</span>
-    {/* Register Button */}
-    <button class="btn h-[52px] text-base px-8">Buy Now</button>
-  </div>;
+      {/* Separator */}
+      <span class="mx-6 text-white/20 font-thin">|</span>
+      {/* Buy Now Button with Link */}
+      <a href="https://rock-swap.io/#/swap?outputCurrency=0x2ae505d947254bd7fde48799cc388215348e1e23">
+        <button class="btn h-[52px] text-base px-8">Buy Now</button>
+      </a>
+    </div>
+  );
 };
+
 
 export default AccountButtons;
